de4fabfd4c36577108112705884b62c1ce43379b
Brian Michalski <bmichalski@gmail.com>
2023-05-02 23:54:27 -0400

    Clean up require.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,13 @@
-require "securerandom"
-
 class Photo < ApplicationRecord
   has_one_attached :photo do |attachable|
     attachable.variant :medium, resize_to_limit: [960, 960]
   end
 
-  before_create { self.uuid = SecureRandom.uuid if uuid.blank? }
+  before_create do
+    require "securerandom"
+
+    self.uuid = SecureRandom.uuid if uuid.blank?
+  end
 
   def to_param
     uuid
