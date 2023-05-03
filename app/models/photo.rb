0db635488b6d866c35832e0577584bcdb066df2e
Brian Michalski <bmichalski@gmail.com>
2023-05-02 23:37:48 -0400

    Fix errors on photo creation.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,17 @@
+require "securerandom"
+
 class Photo < ApplicationRecord
   has_one_attached :photo do |attachable|
     attachable.variant :medium, resize_to_limit: [960, 960]
   end
 
+  before_create { self.uuid = SecureRandom.uuid if uuid.blank? }
+
   def to_param
     uuid
+  end
+
+  def has_location?
+    (!latitude.blank? && !longitude.blank? && latitude != 0 && longitude != 0)
   end
 end
