7020849f8fb8f1a4c515d50c27397d3e6f714a9a
Brian Michalski <bmichalski@gmail.com>
2023-05-15 00:43:36 -0400

    First try to fetch oauth config from ENV.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1017266744995-k9heo6bk78idvi7qu9kvs0a6ntrfj1mg.apps.googleusercontent.com', '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'
+  provider :google_oauth2, ENV.fetch('GOOGLE_OAUTH_CLIENTID', '1017266744995-k9heo6bk78idvi7qu9kvs0a6ntrfj1mg.apps.googleusercontent.com'), ENV.fetch('GOOGLE_OAUTH_SECRET', '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')
 end
